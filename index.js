cd20f0b6fb3a8dcbd5b819f3ed4c9a46df5b6646
fraserxu <xvfeng123@gmail.com>
2013-12-29 21:04:45 +0800

    refacotr code structure

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 var es = require('event-stream')
-var guitl = require('gulp-util')
+var gutil = require('gulp-util')
 var compile = require('./lib/compile')
 
 module.exports = function(options) {
   options = options || {}
 
   return es.map(function(file, cb) {
-    file.contents = file.contents.toString()
+    options.contents = file.contents.toString()
 
     compile(file.path, options, function(err, data) {
       if (err) {
         return cb(err)
       }
 
-      gutil.log('gulp-html2js:\n' + data[0].output.join('\n'))
+      gutil.log('gulp-html2js write: ' + data)
       cb(null, file)
     })
   })
