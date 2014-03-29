4f6b087ec36829ee6e46ebfe623613e5af3cc4e4
fraserxu <xvfeng123@gmail.com>
2014-03-29 11:34:17 +0800

    update dependencies

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var es = require('event-stream')
+var map = require('map-stream')
 var gutil = require('gulp-util')
 var compile = require('./lib/compile')
 
 module.exports = function(options) {
   options = options || {}
 
-  return es.map(function(file, cb) {
+  return map(function(file, cb) {
     compile(file, options, function(err, data) {
       if (err) return cb(err)
 
