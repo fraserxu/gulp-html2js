f5a6db273b505e786255f078e7e0209700fe746e
Flaviu Chelaru <flaviu.chelaru@gmail.com>
2016-02-15 16:18:34 +0200

    + Changed structure to support adapters - removed dependency on gulp-concat + supports multiple angular templates in a single module - see example + added JS Adapter - this will open the posibility of using the plugin with underscore.js, vue.js and other similar libraries. + Added support for future development of adapters + The demo files are now ignored. Eventually we could provide support in running the examples or put the examples responses to readme.md

--- examples/gulpfile.js
+++ examples/gulpfile.js
@@ -2,12 +2,20 @@ var gulp = require('gulp')
 var concat = require('gulp-concat')
 var html2js = require('../')
 
-gulp.task('default', function() {
-  gulp.src('templates/*.html')
-    .pipe(html2js({
-      outputModuleName: 'template-test',
-      useStrict: true
-    }))
-    .pipe(concat('template.js'))
-    .pipe(gulp.dest('dist/'))
-})
+gulp.task('default', function () {
+    gulp.src('templates/*.html')
+        .pipe(html2js('js-demo.js', {
+            adapter: 'javascript',
+            name: 'demo',
+            base: 'templates'
+        }))
+        .pipe(gulp.dest('dist/'))
+
+    gulp.src('templates/*.html')
+        .pipe(html2js('angular.js', {
+            adapter: 'angular',
+            name: 'angular-demo'
+        }))
+        .pipe(gulp.dest('dist/'))
+});
+
--- index.js
+++ index.js
@@ -1,17 +1,78 @@
-var map = require('map-stream')
-var gutil = require('gulp-util')
-var compile = require('./lib/compile')
+'use strict';
 
-module.exports = function(options) {
-  options = options || {}
+var through = require('through2');
+var path = require('path');
+var gutil = require('gulp-util');
+var htmlToJs = require('./lib/compile');
+var PluginError = gutil.PluginError;
+var File = gutil.File;
+const PLUGIN_NAME = 'gulp-html2js';
 
-  return map(function(file, cb) {
-    compile(file, options, function(err, data) {
-      if (err) return cb(err)
+// file can be a vinyl file object or a string
+// when a string it will construct a new one
+module.exports = function (file, opt) {
+    if (!file) {
+        throw new PluginError(PLUGIN_NAME, 'Missing file option for gulp-html-to-js');
+    }
+    var opt = opt || {};
+    opt.base = opt.base || '.'
+    opt.quoteChar = opt.quoteChar || '"'
+    opt.indentString = opt.indentString || '  '
+    opt.target = opt.target || ''
+    opt.htmlmin = opt.htmlmin || {}
+    opt.useStrict = opt.useStrict || false
+    opt.outputModuleName = opt.outputModuleName || false;
+    opt.separator = gutil.linefeed;
 
-      file.contents = new Buffer(data)
+    var _module,
+        latestFile,
+        latestMod;
 
-      cb(null, file)
-    })
-  })
-}
+    return through.obj(function (file, enc, cb) {
+        // ignore empty files
+        if (file.isNull()) {
+            cb();
+            return;
+        }
+        if (!latestMod || file.stat && file.stat.mtime > latestMod) {
+            latestFile = file;
+            latestMod = file.stat && file.stat.mtime;
+        }
+
+        // we don't do streams (yet)
+        if (file.isStream()) {
+            this.emit('error', new PluginError(PLUGIN_NAME, 'Streaming not supported'));
+            cb();
+            return;
+        }
+        // construct concat instance
+        if (!_module) {
+            _module = new htmlToJs(opt);
+        }
+        // add file to concat instance
+        _module.add(file.path, file.contents.toString());
+        cb();
+    }, function (cb) {
+        // no files passed in, no file goes out
+        if (!latestFile || !_module) {
+            cb();
+            return;
+        }
+
+
+        var joinedFile;
+
+        // if file opt was a file path
+        // clone everything from the latest file
+        if (typeof file === 'string') {
+            joinedFile = latestFile.clone({contents: false});
+            joinedFile.path = path.join(latestFile.base, file);
+        } else {
+            joinedFile = new File(file);
+        }
+
+        joinedFile.contents = _module.content;
+        this.push(joinedFile);
+        cb();
+    });
+};
