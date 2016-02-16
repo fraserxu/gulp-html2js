968aac5085f5942975bda2478b3119b1d1d8efc0
Diego <diego@vieira.uk>
2016-02-16 13:44:12 -0200

    Update gulpfile.js
    we don't depend on concat anymore

--- examples/gulpfile.js
+++ examples/gulpfile.js
@@ -1,6 +1,5 @@
-var gulp = require('gulp')
-var concat = require('gulp-concat')
-var html2js = require('../')
+var gulp = require('gulp');
+var html2js = require('../');
 
 gulp.task('default', function () {
     gulp.src('templates/*.html')
@@ -9,13 +8,12 @@ gulp.task('default', function () {
             name: 'demo',
             base: 'templates'
         }))
-        .pipe(gulp.dest('dist/'))
+        .pipe(gulp.dest('dist/'));
 
     gulp.src('templates/*.html')
         .pipe(html2js('angular.js', {
             adapter: 'angular',
             name: 'angular-demo'
         }))
-        .pipe(gulp.dest('dist/'))
+        .pipe(gulp.dest('dist/'));
 });
-
